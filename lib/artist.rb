f7ecdaac76713dea640ebd53fbc4fbe4f59c0885
Daniel Abraham Braude <daniboy1007@gmail.com>
2021-03-28 04:32:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,42 +1,39 @@
-require 'pry'
-
 class Artist
-  attr_accessor :name
-  attr_reader :songs
 
-  @@artists = []
+  attr_accessor :name, :songs
+  @@all =[]
 
-  def initialize
-    @@artists << self
+  def initialize(name)
+    @name = name
     @songs = []
-  end
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
-  def self.all
-    @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
     @songs << song
-    song.artist = self
   end
 
-  def add_songs(songs)
-    songs.each { |song| add_song(song) }
+  def save
+    @@all << self #saves the artist name to the all array
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
+  def self.all
+    @@all #displays the all array
+  end
+
+  def self.find_or_create_by_name(artist_name)
+    found_artist = self.all.find {|artist| artist.name == artist_name}
+    if found_artist
+      found_artist
+    else
+      new_artist = self.new(artist_name)
+      new_artist.save
+      new_artist
+    end
+  end
+
+  def print_songs
+    @songs.each do |song|
+      puts song.name
+    end
   end
 end
